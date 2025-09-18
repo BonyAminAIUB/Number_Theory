c82e50393ad3879ad82df9f5de53fd1b0a485d3d
Md Bony Amin <24-56311-1@student.aiub.edu>
2025-09-18 19:03:40 +0600

    Add files via upload

---
+++ prime_number.cpp
@@ -0,0 +1,23 @@
+// codeforces prime number problem
+// TLE khabe time complexity
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int flag = 1; 
+    for(int i = 2; i<n; i++){
+        if(n % i == 0){
+            flag = 0;
+            break;
+        }
+    } 
+    if(flag == 1){
+        cout << "Prime" << endl;
+    }   
+    else{
+        cout << "Not Prime" << endl;
+    }
+    return 0;
+}
